a02b3f6c55fed00ac05a79783b351a8253d1077d
chririva <c.riva9@studenti.unibg.it>
2018-04-04 21:21:07 +0200

    Initial commit

---
+++ TicTacToe.py
@@ -0,0 +1,14 @@
+#
+# CHRISTIAN RIVA
+# EWA GIERLACH
+#
+#
+
+
+class TicTacToe:
+    def __init__(self, ):
+        player1_name="Player1"
+        player1_name = "Player2"
+        player2_symbol="X"
+        player1_symbol = "O"
+        next_player=0; #0:player1/player2, 1:player1, 2:player2

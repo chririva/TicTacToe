9f8fcec1d558625518b7b75de616e1b53c7a640e
chririva <c.riva9@studenti.unibg.it>
2018-04-05 11:02:05 +0200

    update 3 works

--- Main.py
+++ Main.py
@@ -7,41 +7,51 @@ from TicTacToe import TicTacToe
 from tabulate import tabulate
 import time
 
-print("TIC TAC TOE GAME")
+print("\n ***** TIC TAC TOE GAME *****\n")
 
 game=TicTacToe()
 
 
-#NUMBER OF PLAYERS
+
+
+
+game.set_name_player1(input("Player1, what's your name?"))
+game.set_name_player2(input("Player2, what's your name?"))
+print(game.get_name_player1()+" is playing with sign: " + game.get_symbol_player1())
+print(game.get_name_player2() + " is playing with sign: " + game.get_symbol_player2())
+#time.sleep(1)
+
+
+
+
+
+
 while True:
-    num_players = int(input("How many players?"))
-    if num_players == 1 or num_players == 2:
+    game.print_battlefield()
+    if game.get_next() == 1:
+        while True:
+            choice = int(input(game.get_name_player1() + " it's your turn. put your choice (1-9): "))
+            if game.is_free(choice):
+                game.put_choice(1,choice)
+                break
+            print("That position is already token")
+    else:
+        while True:
+            choice = int(input(game.get_name_player2() + " it's your turn. put your choice (1-9): "))
+            if game.is_free(choice):
+                game.put_choice(2, choice)
+                break
+            print("That position is already token")
+
+    if game.status_of_the_match() != 0:
         break
-    print("You can chose only 1 or 2 players.")
-
-
-if num_players == 1:
-    print("NOT IMPLEMENTED YET")
-elif num_players == 2:
-    game.set_name_player1(input("Player1, what's your name?"))
-    game.set_name_player2(input("Player2, what's your name?"))
-    print(game.get_name_player1()+" is playing with sign: " + game.get_symbol_player1())
-    print(game.get_name_player2() + " is playing with sign: " + game.get_symbol_player2())
-    #time.sleep(1)
-
-
-
 
 game.print_battlefield()
-game.put_choice(2,4)
-game.put_choice(1,5)
-game.put_choice(1,6)
-game.put_choice(1,1)
-game.put_choice(2,2)
-game.put_choice(2,3)
-game.put_choice(1,7)
-game.put_choice(2,8)
-game.put_choice(2,9)
-game.print_battlefield()
 
-print(game.status_of_the_match())
+if game.status_of_the_match() == 1:
+    print(game.get_name_player1()+" won this match.")
+elif game.status_of_the_match() == 2:
+    print(game.get_name_player2() + " won this match.")
+elif game.status_of_the_match() == 3:
+    print("Nobody won this match ¯\_(ツ)_/¯")
+

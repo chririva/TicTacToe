bfc5d7072ae986408283d95659a36362ebbc1e1f
ewwx <37228820+ewwx@users.noreply.github.com>
2018-04-05 07:49:51 +0200

    my version

--- TicTacToe.py
+++ TicTacToe.py
@@ -3,133 +3,84 @@
 # EWA GIERLACH
 #
 #
-from tabulate import tabulate
+
 
 class TicTacToe:
-    def __init__(self, ):
-        self.player1_name = "Player1"
-        self.player2_name = "Player2"
-        self.player1_symbol = 'X'
-        self.player2_symbol = 'O'
-        self.next_player=0; #0:player1/player2, 1:player1, 2:player2
-        self.battlefield=[['','',''],['','',''],['','','']]
-       
-    def set_name_player1(self,name):
-        self.player1_name = name
+    board = [[" " for i in range(3)] for j in range(3)]
+    player1_name = "Player1"
+    player2_name = "Player2"
+    player1_symbol = "X"
+    player2_symbol = "O"
+    next_player=0 #0:player1/player2, 1:player1, 2:player2
 
-    def set_name_player2(self,name):
-        self.player2_name = name
+    def ask_for_name(self, name1, name2):
+            self.player1_name = name1
+            self.player2_name = name2
 
-    def get_name_player1(self):
-        return self.player1_name
-
-    def get_name_player2(self):
-        return self.player2_name
-
-    def get_symbol_player1(self):
-        return self.player1_symbol
-
-    def get_symbol_player2(self):
-        return self.player2_symbol
-
-    def set_symbol1(self,symbol):
-        changed = True
-        if symbol != self.player2_symbol:
-            self.player1_symbol = symbol
+    def set_position(self,symbol):
+        print("Where to put your symbol?")
+        x = int(input(""))
+        y = int(input(""))
+        if self.board[x][y] == " ":
+            self.board[x][y] = symbol
+            return 1
         else:
-            changed = False
-        return changed
-    def set_symbol2(self,symbol):
-        changed = True
-        if symbol != self.player1_symbol:
-            self.player2_symbol = symbol
-        else:
-            changed = False
-        return changed
-    def print_battlefield(self):
-        print(tabulate(self.battlefield,tablefmt="grid"))
+            print("Wrong position! Try again\n")
+            self.set_position(self)
+            return 0
 
-    def is_free(self,location):
-        free = True
-        if location == 1 and self.battlefield[0][0] != '':
-            free = False
-        elif location == 2 and self.battlefield[0][1] != '':
-            free = False
-        elif location == 3 and self.battlefield[0][2] != '':
-            free = False
-        elif location == 4 and self.battlefield[1][0] != '':
-            free = False
-        elif location == 5 and self.battlefield[1][1] != '':
-            free = False
-        elif location == 6 and self.battlefield[1][2] != '':
-            free = False
-        elif location == 7 and self.battlefield[2][0] != '':
-            free = False
-        elif location == 8 and self.battlefield[2][1] != '':
-            free = False
-        elif location == 9 and self.battlefield[2][2] != '':
-            free = False
-        return free
+    def print_board(self):
+        for i in self.board:
+            for j in i:
+                print('|'+j, end='', flush=True)
+            print('|\n')
+
+    def the_end(self):
+        a = self.board
+        if a[0][0] == 'O' and a[1][1] == 'O' and a[2][2] == 'O':
+            print("Congratulations " + self.player2_name + "! You won!")
+            return 0
+        if a[2][0] == 'O' and a[1][1] == 'O' and a[0][2] == 'O':
+            print("Congratulations " + self.player2_name + "! You won!")
+            return 0
+        if a[0][0] == 'X' and a[1][1] == 'X' and a[2][2] == 'X':
+            print("Congratulations " + self.player1_name + "! You won!")
+            return 0
+        if a[2][0] == 'X' and a[1][1] == 'X' and a[0][2] == 'X':
+            print("Congratulations " + self.player1_name + "! You won!")
+            return 0
+        for i in range(0, 3):
+            if a[0][i] == 'X' and a[1][i] == 'X' and a[2][i] == 'X':
+                print("Congratulations "+self.player1_name+"! You won!")
+                return 0
+            if a[i][0] == 'X' and a[i][1] == 'X' and a[i][2] == 'X':
+                print("Congratulations " + self.player1_name + "! You won!")
+                return 0
+            if a[0][i] == 'O' and a[1][i] == 'O' and a[2][i] == 'O':
+                print("Congratulations " + self.player2_name + "! You won!")
+                return 0
+            if a[i][0] == 'O' and a[i][1] == 'O' and a[i][2] == 'O':
+                print("Congratulations " + self.player2_name + "! You won!")
+                return 0
+        return 1
+
+    def game(self):
+        text1 = "Welcome first player! What's your name?\n"
+        text2 = "Welcome second player! What's your name?\n"
+        self.ask_for_name(input(text1), input(text2))
+        print(self.player1_name + " your turn!")
+        self.set_position(self.player1_symbol)
+        self.print_board()
+        while self.the_end():
+            print(self.player2_name + " your turn!")
+            self.set_position(self.player2_symbol)
+            self.print_board()
+            if self.the_end():
+                print(self.player1_name+ " your turn!")
+                self.set_position(self.player1_symbol)
+                self.print_board()
 
 
-    def put_choice(self,player,location):
-        symbol_of_the_player=self.player1_symbol
-        if player == 2:
-            symbol_of_the_player = self.player2_symbol
-        if player == self.next_player or self.next_player == 0:
-            if self.is_free(location):
-                if location == 1:
-                    self.battlefield[0][0] = symbol_of_the_player
-                elif location == 2:
-                    self.battlefield[0][1] = symbol_of_the_player
-                elif location == 3:
-                    self.battlefield[0][2] = symbol_of_the_player
-                elif location == 4:
-                    self.battlefield[1][0] = symbol_of_the_player
-                elif location == 5:
-                    self.battlefield[1][1] = symbol_of_the_player
-                elif location == 6:
-                    self.battlefield[1][2] = symbol_of_the_player
-                elif location == 7:
-                    self.battlefield[2][0] = symbol_of_the_player
-                elif location == 8:
-                    self.battlefield[2][1] = symbol_of_the_player
-                elif location == 9:
-                    self.battlefield[2][2] = symbol_of_the_player
-
-    def status_of_the_match(self):
-        #0: in game
-        #1: player 1 won
-        #2: player 2 won
-        #3: withdraw
-        winner_symbol = ''
-        result = 3
-
-        if self.battlefield[0][0] == self.battlefield[0][1] and self.battlefield[0][1] == self.battlefield[0][2] and self.battlefield[0][0] !='':
-            winner_symbol = self.battlefield[0][0]
-        if self.battlefield[1][0] == self.battlefield[1][1] and self.battlefield[1][1] == self.battlefield[1][2] and self.battlefield[1][0] !='':
-            winner_symbol = self.battlefield[1][0]
-        if self.battlefield[2][0] == self.battlefield[2][1] and self.battlefield[2][1] == self.battlefield[2][2] and self.battlefield[2][0] !='':
-            winner_symbol = self.battlefield[2][0]
-        if self.battlefield[0][0] == self.battlefield[1][0] and self.battlefield[1][0] == self.battlefield[2][0] and self.battlefield[0][0] !='':
-            winner_symbol = self.battlefield[0][0]
-        if self.battlefield[0][1] == self.battlefield[1][1] and self.battlefield[1][1] == self.battlefield[2][1] and self.battlefield[0][1] !='':
-            winner_symbol = self.battlefield[0][1]
-        if self.battlefield[0][2] == self.battlefield[1][2] and self.battlefield[1][2] == self.battlefield[2][2] and self.battlefield[0][2] !='':
-            winner_symbol = self.battlefield[0][2]
-        if self.battlefield[0][0] == self.battlefield[1][1] and self.battlefield[1][1] == self.battlefield[2][2] and self.battlefield[0][0] !='':
-            winner_symbol = self.battlefield[0][0]
-        if self.battlefield[0][2] == self.battlefield[1][1] and self.battlefield[1][1] == self.battlefield[2][0] and self.battlefield[0][2] !='':
-            winner_symbol = self.battlefield[0][2]
-
-        if winner_symbol != '':
-            if winner_symbol==self.player1_symbol:
-                result=1
-            elif winner_symbol==self.player2_symbol:
-                result=2
-        else: #withdraw or in game?
-            for x in range(1, 10):
-                if self.is_free(x):
-                    result = 0
-
-        return result
+if __name__ == '__main__':
+    game1 = TicTacToe()
+    game1.game()

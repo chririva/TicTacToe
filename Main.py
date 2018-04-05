af2aa1e934ed596dfbc6478f12279ce734c56a53
ewwx <37228820+ewwx@users.noreply.github.com>
2018-04-05 12:45:37 +0500

    a minor adjustment

--- Main.py
+++ Main.py
@@ -4,27 +4,13 @@
 #
 #
 from TicTacToe import TicTacToe
-from tabulate import tabulate
-import time
-
 print("\n ***** TIC TAC TOE GAME *****\n")
 
-game=TicTacToe()
-
-
-
-
-
+game = TicTacToe()
 game.set_name_player1(input("Player1, what's your name?"))
 game.set_name_player2(input("Player2, what's your name?"))
 print(game.get_name_player1()+" is playing with sign: " + game.get_symbol_player1())
 print(game.get_name_player2() + " is playing with sign: " + game.get_symbol_player2())
-#time.sleep(1)
-
-
-
-
-
 
 while True:
     game.print_battlefield()
@@ -32,16 +18,16 @@ while True:
         while True:
             choice = int(input(game.get_name_player1() + " it's your turn. put your choice (1-9): "))
             if game.is_free(choice):
-                game.put_choice(1,choice)
+                game.put_choice(1, choice)
                 break
-            print("That position is already token")
+            print("That position is already taken")
     else:
         while True:
             choice = int(input(game.get_name_player2() + " it's your turn. put your choice (1-9): "))
             if game.is_free(choice):
                 game.put_choice(2, choice)
                 break
-            print("That position is already token")
+            print("That position is already taken")
 
     if game.status_of_the_match() != 0:
         break
